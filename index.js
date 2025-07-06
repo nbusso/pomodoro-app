95f7556509b6be42b15b542a96a1ba1aa4b437cd
Nicolás Busso <nicolasbusso@gmail.com>
2025-07-06 01:19:59 -0300

    completed cicles logic

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const configModal = document.getElementById('config-modal')
 const configModalForm = document.getElementById('config-modal-form')
 
-let sessionTimeData = 25
-let breakTimeData = 5
-let pomodoroCount = 4
+let sessionTimeData = parseFloat(document.getElementById('session-time').value, 10)
+let breakTimeData = parseFloat(document.getElementById('break-time').value, 10)
+let pomodoroCount = parseFloat(document.getElementById('pomodoros').value, 10)
+let sessionType = document.getElementById('session-type')
+let cicloActual = 1
 let tiempoRestante = sessionTimeData * 60
+let tiempoRestanteBreak = breakTimeData * 60
+let isBreak = false
 
 let intervalo
+let intervaloBreak
 
 document.addEventListener("click", function(e){
     if (e.target.closest('#config-btn') || e.target.closest('#config-close-btn')) {
@@ -34,24 +39,35 @@ function mostrarConfig() {
 configModalForm.addEventListener('submit', function(e) {
     e.preventDefault()
 
-    sessionTimeData = parseInt(document.getElementById('session-time').value, 10)
-    breakTimeData = parseInt(document.getElementById('break-time').value, 10)
-    pomodoroCount = parseInt(document.getElementById('pomodoros').value, 10)
+    sessionTimeData = parseFloat(document.getElementById('session-time').value, 10)
+    breakTimeData = parseFloat(document.getElementById('break-time').value, 10)
+    pomodoroCount = parseFloat(document.getElementById('pomodoros').value, 10)
     tiempoRestante = sessionTimeData * 60
 
     document.getElementById('config-modal').classList.toggle('hidden')
     
+    mostrarConfig()
     renderTimer()
 })
 
 function renderTimer() {
+        sessionType.textContent = "Estudio..."
         let minutos = Math.floor(tiempoRestante / 60)
         let segundos = tiempoRestante % 60;
         let display = `${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, 0)}`
         document.getElementById('timer-display').textContent = display;
 }
 
+function renderBreakTimer() {
+        sessionType.textContent = "Break..."
+        let minutos = Math.floor(tiempoRestanteBreak / 60)
+        let segundos = tiempoRestanteBreak % 60;
+        let display = `${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, 0)}`
+        document.getElementById('timer-display').textContent = display;
+}
+
 function iniciar() {
+    console.log('Iniciando sesión estudio....')
     intervalo = setInterval(() => {
         tiempoRestante--
 
@@ -60,23 +76,55 @@ function iniciar() {
         if (tiempoRestante <= 0) {
             clearInterval(intervalo)
             intervalo = null
+
+            if (cicloActual <= pomodoroCount) {
+                tiempoRestante = sessionTimeData * 60
+                iniciarBreak()
+            }
         }
     }, 1000)
     document.getElementById('pause-btn').disabled = false
     document.getElementById('reset-btn').disabled = false
 }
 
+function iniciarBreak() {
+    console.log('Iniciando Break...')
+    intervaloBreak = setInterval(() => {
+        tiempoRestanteBreak--
+
+        renderBreakTimer()
+
+        if (tiempoRestanteBreak <= 0) {
+            clearInterval(intervaloBreak)
+            intervaloBreak = null
+
+            if (cicloActual < pomodoroCount) {
+                cicloActual++
+                tiempoRestanteBreak = breakTimeData * 60
+                iniciar()
+            } else {
+                sessionType.textContent = "Pomodoros Completados!"
+            }
+        }
+    }, 1000)
+}
+
 function pausar() {
     clearInterval(intervalo)
+    clearInterval(intervaloBreak)
     intervalo = null
+    intervaloBreak = null
 }
 
 
 function reset() {
     clearInterval(intervalo)
+    clearInterval(intervaloBreak)
     intervalo = null
+    intervaloBreak = null
 
     tiempoRestante = sessionTimeData * 60
+    tiempoRestanteBreak = breakTimeData * 60
 
     document.getElementById('pause-btn').disabled = true
     document.getElementById('reset-btn').disabled = true
@@ -85,3 +133,4 @@ function reset() {
 }
 
 mostrarConfig()
+renderTimer()
